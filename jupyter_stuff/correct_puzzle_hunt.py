2fe6a8c0ae800fd2f06e15ca9e6f62c56d96933d
Zev Minsky-Primus <zevminskyprimus@gmail.com>
2024-12-30 17:01:36 -0500

    more progress

--- jupyter_stuff/correct_puzzle_hunt.py
+++ jupyter_stuff/correct_puzzle_hunt.py
@@ -1,8 +1,11 @@
 import checker
+import getpass
 
 
 def run_puzzle_1(f, input):
     def puzzle_1(var_int):
         return chr(var_int + 3)
 
-    checker.attempt_solution("some_user", "puzzle_1", f, puzzle_1, [10, 2, 83], input)
+    checker.attempt_solution(
+        getpass.getuser(), "puzzle_1", f, puzzle_1, [10, 2, 83], input
+    )
