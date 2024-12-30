c113c024cb0d669fd4fcd7744b9d6d66dcfdcf8c
Zev Minsky-Primus <zevminskyprimus@gmail.com>
2024-12-30 17:24:08 -0500

    okay I think this is pretty good

--- jupyter_stuff/checker.py
+++ jupyter_stuff/checker.py
@@ -23,7 +23,7 @@ def is_correct_on_input(user_f: Callable, correct_f: Callable, input: Any) -> bo
 
 
 def is_correct_on_inputs(
-    user_f: Callable, correct_f: Callable, example_inputs: list[Any]
+    user_f: Callable, correct_f: Callable, example_inputs: set[Any]
 ) -> bool:
     return all(
         is_correct_on_input(user_f, correct_f, input) for input in example_inputs
@@ -34,11 +34,17 @@ def attempt_solution(
     puzzle_name: str,
     user_f: Callable,
     correct_f: Callable,
-    example_inputs: list[Any],
+    example_inputs: set[Any],
+    expected_real_input: Any,
     real_input: Any,
 ):
+    example_inputs.add(expected_real_input)
     if is_correct_on_inputs(user_f, correct_f, example_inputs):
-        leaderboard.update_leaderboard(user, puzzle_name)
+        if expected_real_input == real_input:
+            leaderboard.update_leaderboard(user, puzzle_name)
+            print("\033[32mCorrect!\033[0m")
+        else:
+            print(f"\033[31mIncorrect Input. Expected {expected_real_input}\033[0m")
         print(f"\033[32m{user_f(real_input)}\033[0m")
     else:
         print("\033[31mIncorrect :(\033[0m")
--- jupyter_stuff/correct_puzzle_hunt.py
+++ jupyter_stuff/correct_puzzle_hunt.py
@@ -6,4 +6,4 @@ def run_puzzle_1(f, input):
     def puzzle_1(var_int):
         return chr(var_int + 3)
 
-    checker.attempt_solution("puzzle_1", f, puzzle_1, [10, 2, 83], input)
+    checker.attempt_solution("puzzle_1", f, puzzle_1, {2, 83}, 10, input)
