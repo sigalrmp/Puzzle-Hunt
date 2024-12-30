00e15796e568ed59e0fc454c1642400ddfe29c11
Zev Minsky-Primus <zevminskyprimus@gmail.com>
2024-12-30 17:47:53 -0500

    Update setup.sh

--- jupyter_stuff/setup.sh
+++ jupyter_stuff/setup.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 sudo yum install python pip
-pip install jupyterlab jupyterhub dockerspawner
+sudo pip install jupyterlab jupyterhub dockerspawner jupyterhub-dockerspawner
 
 sudo yum install -y docker
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo systemctl start docker
 
+sudo pip install --upgrade docker --user
+
 curl -sL https://rpm.nodesource.com/setup_20.x | sudo bash -
 sudo yum install -y nodejs
 sudo npm install -g configurable-http-proxy
+
+sudo docker network create jupyterhub
