951c14a1f180ca62a191bf98b119f0441440b284
Zev Minsky-Primus <zevminskyprimus@gmail.com>
2024-12-30 18:13:11 -0500

    Update setup.sh

--- jupyter_stuff/setup.sh
+++ jupyter_stuff/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo yum install python pip
-sudo pip install jupyterlab jupyterhub dockerspawner jupyterhub-dockerspawner
+sudo pip install jupyterlab jupyterhub dockerspawner jupyterhub-dockerspawner tabulate
 
 sudo yum install -y docker
 sudo systemctl start docker
